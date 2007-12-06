313acc774bff2f76f41ca75cbca43ff3f0ad20cc
Richard Kettlewell <rjk@greenend.org.uk>
2007-12-06 19:08:41 +0000

    update (c) messages for mark & ross's contributions

--- clients/playrtp-oss.c
+++ clients/playrtp-oss.c
@@ -1,6 +1,7 @@
 /*
  * This file is part of DisOrder.
  * Copyright (C) 2007 Richard Kettlewell
+ * Portions copyright (C) 2007 Ross Younger
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
