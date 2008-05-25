3c03f5572c03f41c268ec045bbd4a4d39afb80b1
Richard Kettlewell <rjk@greenend.org.uk>
2008-05-25 20:35:45 +0100

    Catch up with new CGI location

--- cgi/disorder-cgi.h
+++ cgi/disorder-cgi.h
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA
  */
-/** @file server/disorder-cgi.h
+/** @file cgi/disorder-cgi.h
  * @brief Shared header for DisOrder CGI program
  */
 
