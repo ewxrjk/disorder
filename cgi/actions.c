5f2d537cfc874929ae2bea8f048f23bd77476ad5
Richard Kettlewell <rjk@greenend.org.uk>
2008-06-14 20:39:50 +0100

    typo fix

--- cgi/actions.c
+++ cgi/actions.c
@@ -434,7 +434,7 @@ static void act_register(void) {
     return;
   }
   /* We could well do better address validation but for now we'll just do the
-   * minimum
+   * minimum */
    /* TODO use email_valid() */
   if(!strchr(email, '@')) {
     login_error("bademail");
