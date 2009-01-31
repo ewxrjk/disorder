8484d1bf363e3f2f45e28939aca71f5676e57c81
Richard Kettlewell <rjk@greenend.org.uk>
2009-01-31 12:29:04 +0000

    Disobedience updates last_playing whenever a 'playing' command completes now.  This mostly keeps the played so far field correct though it does briefly jump to a wrong value and then back when you unpause.
    http://code.google.com/p/disorder/issues/detail?id=26

--- disobedience/queue.c
+++ disobedience/queue.c
@@ -41,8 +41,6 @@ static void playing_completed(void *v,
 
 /** @brief Called when either the actual queue or the playing track change */
 static void queue_playing_changed(void) {
-  const char *old_id = playing_track ? playing_track->id : 0;
-  
   /* Check that the playing track isn't in the queue.  There's a race here due
    * to the fact that we issue the two commands at slightly different times.
    * If it goes wrong we re-issue and try again, so that we never offer up an
@@ -68,8 +66,6 @@ static void queue_playing_changed(void) {
     playing_track = NULL;
     q = actual_queue;
   }
-  if(!old_id || !playing_track || strcmp(old_id, playing_track->id))
-    time(&last_playing);                /* for column_length() */
   ql_new_queue(&ql_queue, q);
   /* Tell anyone who cares */
   event_raise("queue-list-changed", q);
@@ -98,6 +94,7 @@ static void playing_completed(void attribute((unused)) *v,
   }
   actual_playing_track = q;
   queue_playing_changed();
+  time(&last_playing);
 }
 
 /** @brief Schedule an update to the queue
