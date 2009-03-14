0a829a955581097cbf268b36e7c5464412025065
Richard Kettlewell <rjk@greenend.org.uk>
2009-03-14 11:33:35 +0000

    Horrid hack to recover run-time endianness for testing.

---
+++ server/endian.c
@@ -0,0 +1,20 @@
+#include <config.h>
+#include <stdio.h>
+
+int main(void) {
+#if WORDS_BIGENDIAN
+  puts("1");
+#else
+  puts("0");
+#endif
+  return 0;
+}
+
+/*
+Local Variables:
+c-basic-offset:2
+comment-column:40
+fill-column:79
+indent-tabs-mode:nil
+End:
+*/
