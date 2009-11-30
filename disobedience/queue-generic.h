9b44040b416ff423c293b3065864316b58399158
Richard Kettlewell <rjk@greenend.org.uk>
2009-11-30 22:26:38 +0000

    Give the currently playing track a light green background.

--- disobedience/queue-generic.h
+++ disobedience/queue-generic.h
@@ -136,15 +136,8 @@ enum {
   EXTRA_COLUMNS
 };
 
-/* TODO probably need to set "horizontal-separator" to 0, but can't find any
- * coherent description of how to set style properties in isolation. */
-#define BG_PLAYING 0
-#define FG_PLAYING 0
-
-#ifndef BG_PLAYING
-# define BG_PLAYING "#e0ffe0"
-# define FG_PLAYING "black"
-#endif
+#define BG_PLAYING "#e0ffe0"
+#define FG_PLAYING "black"
 
 extern struct queuelike ql_queue;
 extern struct queuelike ql_recent;
